fed1127b2f846ac7f276207cf01379f9446c058e
mads-88 <madisonhickey@u.boisestate.edu>
2026-04-27 23:56:19 -0600

    Update stopwatch.v

--- stopwatch.v
+++ stopwatch.v
@@ -6,6 +6,22 @@ module stopwatch(
     output [5:0] state     //6-bits to represent the highest number 59
 );
     
+reg [5:0] count;
+assign state = count;
+
+always @(posedge clk or posedge rst) begin
+    if (rst)
+        count <= 0;
+    else if (en) begin
+        if (count == 59)
+            count <= 0;
+        else
+            count <= count + 1;
+    end
+    else
+        count <= count;
+end
+    
    
 endmodule
 
